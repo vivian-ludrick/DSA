44b53779e692133e0727283e8f53d68d03e783c7
vivian-ludrick <vivianludrick.social+github@gmail.com>
2022-10-27 14:54:46 +0530

    recantangle pattern

--- pattern/rectangle.cpp
+++ pattern/rectangle.cpp
@@ -17,12 +17,16 @@ cin>>rows;
 cout<<"Number of columns     :   ";
 cin>>columns;
 
+
+// looping for the no of rows
 for (int i = 0; i < rows; i++)
 {
+    // looping for no of columns
     for (int j = 0; j < columns; j++)
     {
         cout<<" * ";
     }
+    // to print pattern on a new line
     cout<<endl;
 }
 
