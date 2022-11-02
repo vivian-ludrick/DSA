b27e31dd852af61bb56119755fac510ff4bccc4d
vivian-ludrick <vivianludrick.social+github@gmail.com>
2022-11-02 12:08:44 +0530

    comments are updated

--- array/reverse_an_array.cpp
+++ array/reverse_an_array.cpp
@@ -17,22 +17,26 @@ for(int i = 0; i < size; i++){
     cin >> arr[i];
 }
 
-int j =size;
+// j is initialized outside the loop so that it doesn't get the same value evertime the loop works
+int j = size;
+// creating a dummy variable to swap values
 int temp;
 
+// To swap the opposite elements. 
+// Looping half the size so the values don't get repeated
 for (int i = 0; i < size/2; i++)
 {
+    // As i increases j should decrease ajacently
     j--;
     
+    // Swaping the elements
     temp = arr[j];
     arr[j]= arr[i];
     arr[i] = temp; 
 }
 
 
-
-
-
+// Printing the reversed array
 for(int i = 0; i < size; i++){
     cout << arr[i]<<endl;
 }
@@ -40,3 +44,6 @@ for(int i = 0; i < size; i++){
 
 return 0;
 }
+
+
+// At line 27 the size is divide by 2 so that the swap occurs efficiently. If the size is odd then the middle element wouldn't be swaped and if it is even then the two middle elements would be swapped. It wouldn't throw an error since the loop is only <size and not <=size
